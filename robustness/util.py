884111db683a0c8ac269d18ff10df042bb6247fe
rpytel1 <rafal.pytel1@wp.pl>
2019-04-19 16:34:11 +0200

    Updated robustness check with nicer graphics

--- robustness/util.py
+++ robustness/util.py
@@ -2,6 +2,7 @@ import copy
 import math
 import operator
 from collections import OrderedDict
+from random import shuffle
 
 import networkx as nx
 import pandas as pd
@@ -51,6 +52,50 @@ def get_nodes_ordered_by_betweeness(graph, reverse=True):
     return list(ordered_clustering.keys())
 
 
+def get_nodes_ordered_by_reputation(graph):
+    df = pd.read_csv('data/mathoverflow/mathoverflow_dataset.csv')
+    reputation_dict = dict(zip(df.UserId, df.Reputation))
+
+    reputation_dict = OrderedDict(sorted(reputation_dict.items()))
+
+    temp_reputation_dict = {k: v for k, v in reputation_dict.items() if str(k) in graph.nodes}
+    temp_reputation_dict = OrderedDict(sorted(temp_reputation_dict.items(), key=operator.itemgetter(1), reverse=True))
+    #
+    std = np.std(list(temp_reputation_dict.values()))
+    max_value = max(list(temp_reputation_dict.values()))
+    i = 0
+    for v in temp_reputation_dict.values():
+        if v > max_value - 3 * std:
+            i += 1
+
+    return list(temp_reputation_dict.keys()), i / len(graph.nodes())
+
+
+def get_nodes_ordered_by_upvotes(graph):
+    df = pd.read_csv('data/mathoverflow/mathoverflow_dataset.csv')
+    reputation_dict = dict(zip(df.UserId, df.UpVotes))
+
+    reputation_dict = OrderedDict(sorted(reputation_dict.items()))
+
+    temp_reputation_dict = {k: v for k, v in reputation_dict.items() if str(k) in graph.nodes}
+    temp_reputation_dict = OrderedDict(sorted(temp_reputation_dict.items(), key=operator.itemgetter(1), reverse=True))
+    #
+    std = np.std(list(temp_reputation_dict.values()))
+    max_value = max(list(temp_reputation_dict.values()))
+    i = 0
+    for v in temp_reputation_dict.values():
+        if v > max_value - 3 * std:
+            i += 1
+
+    return list(temp_reputation_dict.keys()), i / len(graph.nodes())
+
+
+def get_random_ranking(graph):
+    nodes = graph.nodes
+    shuffle(nodes)
+    return nodes
+
+
 def get_failed_nodes(graph, fraction, nodes_ranking):
     final_id = math.floor(fraction * len(graph.nodes()))
     return nodes_ranking[:final_id]
@@ -90,44 +135,25 @@ def plot_graph_by_attr(G, attr, pos):
     nodes_attr_on = [x for x, y in G.nodes(data=True) if y[attr] == True]
     nodes_attr_off = [x for x, y in G.nodes(data=True) if y[attr] == False]
     # pos = nx.spring_layout(G)
-    nx.draw(G, pos, node_color='g', node_size=100, with_labels=True)
-    nx.draw_networkx_nodes(G, pos, nodelist=nodes_attr_on, node_color='r', node_size=100)
-    nx.draw_networkx_nodes(G, pos, nodelist=nodes_attr_off, node_color='g', node_size=50, label='')
+    nx.draw(G, pos, node_color='g', node_size=50, with_labels=False)
+    nx.draw_networkx_nodes(G, pos, nodelist=nodes_attr_on, node_color='r', node_size=75)
+    nx.draw_networkx_nodes(G, pos, nodelist=nodes_attr_off, node_color='g', node_size=30, label='')
+    print(G.edges)
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges, edge_color='b')
 
 
-def get_nodes_ordered_by_reputation(graph):
-    df = pd.read_csv('data/mathoverflow/mathoverflow_dataset.csv')
-    reputation_dict = dict(zip(df.UserId, df.Reputation))
+# We aim to get 30x std based on degree, which gives around 1.2 %
+def get_fraction(graph):
+    degree_view = list(graph.degree)
+    degree = [elem[1] for elem in degree_view]
 
-    reputation_dict = OrderedDict(sorted(reputation_dict.items()))
-
-    temp_reputation_dict = {k: v for k, v in reputation_dict.items() if str(k) in graph.nodes}
-    temp_reputation_dict = OrderedDict(sorted(temp_reputation_dict.items(), key=operator.itemgetter(1), reverse=True))
-    #
-    std = np.std(list(temp_reputation_dict.values()))
-    max_value = max(list(temp_reputation_dict.values()))
+    limit = max(degree) - 30 * np.std(degree)
     i = 0
-    for v in temp_reputation_dict.values():
-        if v > max_value - 3 * std:
+
+    for elem in degree:
+        if float(elem) > limit:
             i += 1
 
-    return list(temp_reputation_dict.keys()), i / len(graph.nodes())
+    print(i)
 
-
-def get_nodes_ordered_by_upvotes(graph):
-    df = pd.read_csv('data/mathoverflow/mathoverflow_dataset.csv')
-    reputation_dict = dict(zip(df.UserId, df.UpVotes))
-
-    reputation_dict = OrderedDict(sorted(reputation_dict.items()))
-
-    temp_reputation_dict = {k: v for k, v in reputation_dict.items() if str(k) in graph.nodes}
-    temp_reputation_dict = OrderedDict(sorted(temp_reputation_dict.items(), key=operator.itemgetter(1), reverse=True))
-    #
-    std = np.std(list(temp_reputation_dict.values()))
-    max_value = max(list(temp_reputation_dict.values()))
-    i = 0
-    for v in temp_reputation_dict.values():
-        if v > max_value - 3 * std:
-            i += 1
-
-    return list(temp_reputation_dict.keys()), i / len(graph.nodes())
+    return i / len(degree)
